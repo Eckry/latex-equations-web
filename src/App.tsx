6adff96a6a0c6d84fd8c2b3800417d188f6ebe31
Eckry <112786854+Eckry@users.noreply.github.com>
2025-09-01 09:33:26 -0600

    add fontsize instructions

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,27 @@ function App() {
           {buttons.map(({ text, Icon }) => {
             return <Button key={text} text={text} Icon={Icon} />
           })}
+          <li>
+            <div className="fontsize-container">
+              <button className="fontsize-up">
+                <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                  stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l14 0" />
+                </svg>
+              </button>
+              <input value={120} className="fontsize-icon" />
+              <button className="fontsize-down">
+                <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                  stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M12 5l0 14" />
+                  <path d="M5 12l14 0" />
+                </svg>
+              </button>
+            </div>
+            <p>Changes the font-size of the equation rendered.</p>
+          </li>
         </ul>
       </section>
       <section>
